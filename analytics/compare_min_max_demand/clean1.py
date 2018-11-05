ff92aa35559ad4f153b637ab4502d363be93052b
Gabe Fierro <gtfierro225@gmail.com>
2018-11-05 10:42:35 +0800

    remove old

--- analytics/compare_min_max_demand/clean1.py
+++
@@ -1,8 +0,0 @@
-
-def run(m, inobject):
-    data = m.cache.get(inobject).copy()
-
-    if len(data) == 0:
-        return None
-
-    return inobject
--- analytics/compare_min_max_demand/execute1.py
+++
@@ -1,6 +0,0 @@
-def run(m, inobject):
-    if inobject is None:
-        return
-    data = m.cache.get(inobject)
-
-    return data['df'].mean()
